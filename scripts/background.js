a8d36c15bd127b933800c65d444da8771dfdc37a
matthwfrost <matthwfrost@gmail.com>
2024-01-14 21:05:07 +0000

    Sweet treats

--- scripts/background.js
+++ scripts/background.js
@@ -1,8 +1,22 @@
 chrome.contextMenus.onClicked.addListener(clicked);
 
-async function clicked(){
-  const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-  const response = await chrome.tabs.sendMessage(tab.id, {greeting: "clicked"});
+async function clicked(info){
+
+  switch(info.menuItemId){
+    case "readContextMenu":
+      const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+      const response = await chrome.tabs.sendMessage(tab.id, {greeting: "clicked"});
+      // console.log("Clicked detected", info.menuItemId);
+      break;
+    case "playing":
+      const [tabPlay] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+      const res = await chrome.tabs.sendMessage(tabPlay.id, {greeting: "stop"});
+      // console.log("Clicked detected", info.menuItemId);
+      break;
+    default:
+      break;
+  }
+
 }
 
 chrome.runtime.onInstalled.addListener(function () {
@@ -12,3 +26,37 @@ chrome.runtime.onInstalled.addListener(function () {
     contexts: ["selection"]
   });
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.action === true) {
+      playingState();
+    }else if (request.action === false) {
+      stopState();
+    }
+  }
+);
+
+// Assume you have some code where loading starts
+function playingState() {
+  // Change the icon when loading starts
+  chrome.contextMenus.removeAll()
+
+  chrome.contextMenus.create({
+    id: "playing",
+    title: "Stop speaking",
+    contexts: ["all"]
+  });
+  // Perform your loading tasks here
+}
+
+// Assume you have some code where loading finishes
+function stopState() {
+  chrome.contextMenus.removeAll()
+
+  chrome.contextMenus.create({
+    id: "readContextMenu",
+    title: "Listen to selected text ( Takes a second )",
+    contexts: ["selection"]
+  });
+}
--- scripts/content.js
+++ scripts/content.js
@@ -1,70 +1,133 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.greeting === "clicked"){
-      console.log("Clicked detected");
+      // console.log("Clicked detected");
       var selectedText = window.getSelection().toString().trim();
       if (selectedText !== '') {
         if(selectedText.length > 1000){
           alert("Please select less than 1000 characters")
         }else{
-          getSpeech(selectedText);
+          getSpeechElevenLabs(selectedText);
         }
       }
-    }  }
+    }else if (request.greeting === "stop"){
+      // Currently, if you want to stop. You need to refresh the page
+      location.reload();
+      chrome.runtime.sendMessage({action: false});
+    }
+  }
 );
 
-async function getSpeech(text) {
-  try {
-    const openaiApiKey = 'OPENAI_API_KEY';
 
-    const apiUrl = 'https://api.openai.com/v1/audio/speech';
 
-    const requestData = {
-      model: 'tts-1',
-      input: text,
-      voice: 'nova',
-    };
+let source;
+async function getSpeechElevenLabs(text) {
 
-    // Fetch audio data
-    const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${openaiApiKey}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(requestData),
-    });
+  const start = Date.now();
+  (async () => {
+    await chrome.runtime.sendMessage({action: true});
+    // do something with response here, not outside the function
+  })();
 
-    console.log("fetch finished")
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+  let EL_API_KEY = 'API_KEY_HERE';
 
-    const start = Date.now();
+  const options = {
+    method: 'POST',
+    headers: {
+      'xi-api-key': EL_API_KEY, 
+      'Content-Type': 'application/json'
+    },
+    body: `{"text":"${text}", "model_id":"eleven_turbo_v2", "stability":50}`
+  };
 
-    const audioBlob = new Blob([await response.arrayBuffer()], { type: 'audio/mp3' });
-    const audioUrl = URL.createObjectURL(audioBlob);
+  const voices = {
+    old_british_man: "fjUEyxiEBGhIdIzLmVus",
+    lily: "pFZP5JQG7iQjIQuC4Bku",
+    myOwnVoice: "iCFUKc3rB6sfwKZLdamJ",
+  }
 
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const audioElement = new Audio();
-    audioElement.src = audioUrl;
+  fetch(`https://api.elevenlabs.io/v1/text-to-speech/${voices.myOwnVoice}/stream?optimize_streaming_latency=3`, options)
+    .then(response => response.arrayBuffer())
+    .then(data => { 
+      // do something with response here, not outside the function
+      // Play the audio using Web Audio API
+      const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+      audioContext.decodeAudioData(data, buffer => {
+        source = audioContext.createBufferSource();
+        source.buffer = buffer;
+        source.connect(audioContext.destination);
 
-    const source = audioContext.createMediaElementSource(audioElement);
-    source.connect(audioContext.destination);
+          // Listen for the 'ended' event
+        source.addEventListener('ended', () => {
+          // You can perform additional actions or cleanup here
+        chrome.runtime.sendMessage({action: false});
+        // do something with response here, not outside the function
+        });
 
-    audioElement.addEventListener('ended', () => {
-      URL.revokeObjectURL(audioUrl);
-      console.log('API call successful. Audio played.');
-    });
+        source.start();
+      });
 
-    audioElement.play();
-    
     const end = Date.now();
     console.log(`Execution time: ${end - start} ms`);
+    })
+    .catch(error => {
+      console.error('Error fetching and storing data:', error);
+    });
+
+
+}
+
+// async function getSpeechOpenAI(text) {
+//   try {
+
+//     const apiUrl = 'https://api.openai.com/v1/audio/speech';
+
+//     const requestData = {
+//       model: 'tts-1',
+//       input: text,
+//       voice: 'nova',
+//     };
+
+//     // Fetch audio data
+//     const response = await fetch(apiUrl, {
+//       method: 'POST',
+//       headers: {
+//         'Authorization': `Bearer ${openaiApiKey}`,
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(requestData),
+//     });
+
+//     console.log("fetch finished")
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+
+//     const start = Date.now();
+
+//     const audioBlob = new Blob([await response.arrayBuffer()], { type: 'audio/mp3' });
+//     const audioUrl = URL.createObjectURL(audioBlob);
+
+//     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+//     const audioElement = new Audio();
+//     audioElement.src = audioUrl;
+
+//     const source = audioContext.createMediaElementSource(audioElement);
+//     source.connect(audioContext.destination);
+
+//     audioElement.addEventListener('ended', () => {
+//       URL.revokeObjectURL(audioUrl);
+//       console.log('API call successful. Audio played.');
+//     });
+
+//     audioElement.play();
+    
+//     const end = Date.now();
+//     console.log(`Execution time: ${end - start} ms`);
     
 
-    console.log('API call successful. Audio played.');
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
+//     console.log('API call successful. Audio played.');
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// }
