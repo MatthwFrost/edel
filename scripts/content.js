9b39ff80026fc3a11b2a434ecd0139c1585f44ca
matthwfrost <matthwfrost@gmail.com>
2024-03-31 21:27:07 +0100

    Few bug fixes

--- scripts/content.js
+++ scripts/content.js
@@ -54,7 +54,7 @@ function injectAudioPlayer() {
   audioControllerPlayButton = buildAudioControllerPlayButton();
   audioControllerSkipBackwardButton = buildAudioControllerSkipBackwardButton();
   audioControllerSkipForwardButton = buildAudioControllerSkipForwardButton();
-
+  // --- Added to page ---
   audioControllerButtonContainer.appendChild(audioControllerSkipBackwardButton);
   audioControllerButtonContainer.appendChild(audioControllerPlayButton);
   audioControllerButtonContainer.appendChild(audioControllerSkipForwardButton);
@@ -225,7 +225,7 @@ function injectAudioPlayer() {
 }
 
 function buildVolumeButton(){
-  const settingsVolumeButton = document.createElement("button");
+  settingsVolumeButton = document.createElement("button");
   settingsVolumeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M533.6 32.5C598.5 85.2 640 165.8 640 256s-41.5 170.7-106.4 223.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C557.5 398.2 592 331.2 592 256s-34.5-142.2-88.7-186.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM473.1 107c43.2 35.2 70.9 88.9 70.9 149s-27.7 113.8-70.9 149c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C475.3 341.3 496 301.1 496 256s-20.7-85.3-53.2-111.8c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zm-60.5 74.5C434.1 199.1 448 225.9 448 256s-13.9 56.9-35.4 74.5c-10.3 8.4-25.4 6.8-33.8-3.5s-6.8-25.4 3.5-33.8C393.1 284.4 400 271 400 256s-6.9-28.4-17.7-37.3c-10.3-8.4-11.8-23.5-3.5-33.8s23.5-11.8 33.8-3.5zM301.1 34.8C312.6 40 320 51.4 320 64V448c0 12.6-7.4 24-18.9 29.2s-25 3.1-34.4-5.3L131.8 352H64c-35.3 0-64-28.7-64-64V224c0-35.3 28.7-64 64-64h67.8L266.7 40.1c9.4-8.4 22.9-10.4 34.4-5.3z"/></svg>`;
   settingsVolumeButton.style.backgroundColor = "transparent";
   settingsVolumeButton.style.zIndex = "999";
@@ -237,7 +237,7 @@ function buildVolumeButton(){
 }
 
 function buildPlaybackButton(){
-  const settingsPlaybackControllButton = document.createElement("button");
+  settingsPlaybackControllButton = document.createElement("button");
   settingsPlaybackControllButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm320 96c0-26.9-16.5-49.9-40-59.3V88c0-13.3-10.7-24-24-24s-24 10.7-24 24V292.7c-23.5 9.5-40 32.5-40 59.3c0 35.3 28.7 64 64 64s64-28.7 64-64zM144 176a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm-16 80a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM400 144a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>`;
   settingsPlaybackControllButton.style.backgroundColor = "transparent";
   settingsPlaybackControllButton.style.zIndex = "999";
@@ -250,7 +250,7 @@ function buildPlaybackButton(){
 }
 
 function buildSettingsContainer(){
-  const settingsContainer = document.createElement("div");
+  settingsContainer = document.createElement("div");
   settingsContainer.style.display = "flex";
   settingsContainer.style.width = "0"; // Start with minimal width
   settingsContainer.style.overflow = "hidden"; // Prevent content overflow during animation
@@ -266,7 +266,7 @@ function buildSettingsContainer(){
 }
 
 function buildSettingsToggleContainer(){
-  const toggleSettingsContainer = document.createElement("div");
+  toggleSettingsContainer = document.createElement("div");
   toggleSettingsContainer.style.position = "fixed";
   toggleSettingsContainer.style.display = "flex"; // Ensure it's ready to layout its children
   toggleSettingsContainer.style.visibility = "visible"; // Make sure it's not hiding its children
@@ -277,7 +277,7 @@ function buildSettingsToggleContainer(){
 }
 
 function buildSettingsVolumeSlider(){
-  const settingsVolumeSlider = document.createElement("input");
+  settingsVolumeSlider = document.createElement("input");
   settingsVolumeSlider.setAttribute("id", "volume-id");
   settingsVolumeSlider.style.position = "fixed";
   settingsVolumeSlider.style.display = "none";
@@ -303,7 +303,7 @@ function buildSettingsVolumeSlider(){
 }
 
 function buildSettingsPlaybackControllSlider(){
-  const settingsPlaybackControllSlider = document.createElement("input");
+  settingsPlaybackControllSlider = document.createElement("input");
   settingsPlaybackControllSlider.setAttribute("id", "volume-id");
   settingsPlaybackControllSlider.style.position = "fixed";
   settingsPlaybackControllSlider.style.display = "none";
@@ -327,7 +327,7 @@ function buildSettingsPlaybackControllSlider(){
 }
 
 function buildAudioController(){
-  const audioController = document.createElement("div");
+  audioController = document.createElement("div");
   audioController.style.width = "45px";
   audioController.style.height = "40px";
   audioController.style.backgroundColor = "#084b83";
@@ -347,7 +347,7 @@ function buildAudioController(){
 }
 
 function buildAudioControllerButtonContainer(){
-  const audioControllerButtonContainer = document.createElement("div");
+  audioControllerButtonContainer = document.createElement("div");
   audioControllerButtonContainer.style.width = "100%";
   audioControllerButtonContainer.style.height = "100%";
   audioControllerButtonContainer.style.display = "flex";
@@ -357,7 +357,7 @@ function buildAudioControllerButtonContainer(){
 }
 
 function buildAudioControllerPlayButton(){
-  const audioControllerPlayButton = document.createElement("button");
+  audioControllerPlayButton = document.createElement("button");
   audioControllerPlayButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="#fff" height="16" width="16" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>`;
   audioControllerPlayButton.style.display = "flex";
   audioControllerPlayButton.style.alignItems = "center";
@@ -370,7 +370,7 @@ function buildAudioControllerPlayButton(){
 }
 
 function buildAudioControllerSkipBackwardButton(){
-  const audioControllerSkipBackwardButton = document.createElement("button");
+  audioControllerSkipBackwardButton = document.createElement("button");
   audioControllerSkipBackwardButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="#fff" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-skip-back"><polygon points="19 20 9 12 19 4 19 20"/><line x1="5" x2="5" y1="19" y2="5"/></svg>`;
   audioControllerSkipBackwardButton.style.visibility = "hidden";
   audioControllerSkipBackwardButton.style.display = "flex";
@@ -384,7 +384,7 @@ function buildAudioControllerSkipBackwardButton(){
 }
 
 function buildAudioControllerSkipForwardButton(){ 
-  const audioControllerSkipForwardButton = document.createElement("button");
+  audioControllerSkipForwardButton = document.createElement("button");
   audioControllerSkipForwardButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="#fff" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-skip-forward"><polygon points="5 4 15 12 5 20 5 4"/><line x1="19" y1="5" x2="19" y2="19"/></svg>`;
   audioControllerSkipForwardButton.style.display = "flex";
   audioControllerSkipForwardButton.style.visibility = "hidden";
@@ -398,7 +398,7 @@ function buildAudioControllerSkipForwardButton(){
 }
 
 function buildAudioContainer(){
-  const audioContainer = document.createElement("div");
+  audioContainer = document.createElement("div");
   audioContainer.style.position = "fixed";
   audioContainer.style.bottom = screen.height / 2 + "px";
   audioContainer.style.right = "0px";
